26bba4fbc5fac386f56d7f6991dd719a5e90d43d
Alexis Engel <alexisengel99@gmail.com>
2019-08-07 19:01:39 +0800

    final changes for now

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,10 +4,19 @@ class TasksController < ApplicationController
 		@user = User.find(params["user_id"])
 		@all_tasks = []
 		@user_tasks = Task.all.joins(:datasets).where('datasets.user_id = ?', @user.id).uniq.pluck(:id)
-		@not_done_tasks = Task.where.not(id: @user_tasks)
-		@user.tags.each do |tag|
-		 	@all_tasks << @not_done_tasks.joins(:tags).where("tags.id = ?", tag.id)
+		puts params["type"]
+		if params["type"] == "now"
+			@not_done_tasks = Task.where.not(id: @user_tasks)
+			@user.tags.each do |tag|
+		 		@all_tasks << @not_done_tasks.joins(:tags).where("tags.id = ?", tag.id)
+			end
+			@tasks = @all_tasks.flatten.uniq
 		end
-		@tasks = @all_tasks.flatten.uniq
+		if params["type"] == "past"
+			@done_tasks = Task.where(id: @user_tasks)
+			@tasks = @done_tasks.uniq
+
+		end
 	end
 end
+
